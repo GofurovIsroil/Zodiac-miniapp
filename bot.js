5686cfe0af99bfb1ab87073a7ddbdc657793cfd7
GofurovIsroil <isroilgofurov0099@gmail.com>
2024-08-19 02:30:54 +0500

    hopefully last commit

---
+++ bot.js
@@ -0,0 +1,22 @@
+const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
+
+const token = '6683288372:AAHjHsIWWEdMwXhieYUdQ5IN4t9qnW-wA9I';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    const options = {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: 'Открыть WebApp',
+                        web_app: { url: 'https://zodiac-miniapp.vercel.app/' }
+                    }
+                ]
+            ]
+        }
+    };
+    bot.sendMessage(chatId, 'Нажмите кнопку ниже, чтобы открыть WebApp:', options);
+});
--- serverOnly.js
+++ serverOnly.js
@@ -1,6 +1,8 @@
 // serverOnly.js
+
 if (typeof window === 'undefined') {
     // Server-side code
     const TelegramBot = require('node-telegram-bot-api');
     // Use the TelegramBot library here
 }
+
